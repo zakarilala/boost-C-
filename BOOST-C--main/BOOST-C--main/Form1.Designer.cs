0ac0a8c959968d017d26ceaaac3859ae481eb68d
zakarilala <zakarilala.ranaivo@email.com>
2025-06-16 12:38:06 +0300

    My projet Boost C#

---
+++ BOOST-C--main/BOOST-C--main/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Simple_CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonValidate = new System.Windows.Forms.Button();
+            this.dataGridViewPersons = new System.Windows.Forms.DataGridView();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonModify = new System.Windows.Forms.Button();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPersons)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Last Name:";
+            // 
+            // buttonValidate
+            // 
+            this.buttonValidate.Location = new System.Drawing.Point(146, 386);
+            this.buttonValidate.Name = "buttonValidate";
+            this.buttonValidate.Size = new System.Drawing.Size(75, 23);
+            this.buttonValidate.TabIndex = 1;
+            this.buttonValidate.Text = "Validate";
+            this.buttonValidate.UseVisualStyleBackColor = true;
+            this.buttonValidate.Click += new System.EventHandler(this.buttonValidate_Click);
+            // 
+            // dataGridViewPersons
+            // 
+            this.dataGridViewPersons.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPersons.Location = new System.Drawing.Point(357, 56);
+            this.dataGridViewPersons.Name = "dataGridViewPersons";
+            this.dataGridViewPersons.Size = new System.Drawing.Size(397, 310);
+            this.dataGridViewPersons.TabIndex = 2;
+            this.dataGridViewPersons.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewPersons_CellContentClick);
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(155, 126);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(148, 20);
+            this.textBoxLastName.TabIndex = 3;
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(155, 185);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(148, 20);
+            this.textBoxFirstName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "First Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 318);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Address:";
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(155, 311);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(148, 20);
+            this.textBoxAddress.TabIndex = 8;
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Location = new System.Drawing.Point(155, 252);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(148, 20);
+            this.textBoxAge.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Age:";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(500, 386);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 10;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click_1);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(381, 386);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 11;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click_1);
+            // 
+            // buttonModify
+            // 
+            this.buttonModify.Location = new System.Drawing.Point(265, 386);
+            this.buttonModify.Name = "buttonModify";
+            this.buttonModify.Size = new System.Drawing.Size(75, 23);
+            this.buttonModify.TabIndex = 12;
+            this.buttonModify.Text = "Modify";
+            this.buttonModify.UseVisualStyleBackColor = true;
+            this.buttonModify.Click += new System.EventHandler(this.buttonModify_Click_1);
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(606, 386);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(148, 20);
+            this.textBoxId.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(160, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 29);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "BOOST C#";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 433);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.buttonModify);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.textBoxAge);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.dataGridViewPersons);
+            this.Controls.Add(this.buttonValidate);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPersons)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonValidate;
+        private System.Windows.Forms.DataGridView dataGridViewPersons;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonModify;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ BOOST-C--main/BOOST-C--main/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Simple_CRUD
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection connection;
+        private SqlDataAdapter dataAdapter;
+        private DataTable dataTable;
+        private string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\ACER\\source\\repos\\Simple CRUD\\PersonDb.mdf\";Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxId.Visible = false;
+            InitializeDatabaseConnection();
+            LoadPersonsData();
+            ConfigureDataGridView();
+        }
+        private void InitializeDatabaseConnection()
+        {
+            connection = new SqlConnection(connectionString);
+        }
+
+        private void LoadPersonsData()
+        {
+            using (SqlConnection loadConnection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    loadConnection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Persons", loadConnection);
+                    DataTable newDataTable = new DataTable();
+                    adapter.Fill(newDataTable);
+                    dataGridViewPersons.DataSource = newDataTable;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading data: {ex.Message}");
+                }
+            }
+        }
+
+        private void ConfigureDataGridView()
+        {
+            dataGridViewPersons.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridViewPersons.MultiSelect = false;
+            dataGridViewPersons.ReadOnly = true;
+            dataGridViewPersons.RowHeadersVisible = false;
+            dataGridViewPersons.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewPersons.SelectionChanged += DataGridViewPersons_SelectionChanged;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridViewPersons_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void DataGridViewPersons_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridViewPersons.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridViewPersons.SelectedRows[0];
+                textBoxId.Text = selectedRow.Cells["Id"].Value.ToString();
+                textBoxLastName.Text = selectedRow.Cells["LastName"].Value.ToString();
+                textBoxFirstName.Text = selectedRow.Cells["FirstName"].Value.ToString();
+                textBoxAge.Text = selectedRow.Cells["Age"].Value.ToString();
+                textBoxAddress.Text = selectedRow.Cells["Address"].Value?.ToString() ?? string.Empty;
+            }
+        }
+
+        private void buttonValidate_Click(object sender, EventArgs e)
+        {
+            if (ValidateInputs())
+            {
+                try
+                {
+                    using (SqlConnection cmdConnection = new SqlConnection(connectionString))
+                    {
+                        cmdConnection.Open();
+                        SqlCommand cmd = new SqlCommand(
+                            "INSERT INTO Persons (LastName, FirstName, Age, Address) " +
+                            "VALUES (@LastName, @FirstName, @Age, @Address)", cmdConnection);
+
+                        cmd.Parameters.AddWithValue("@LastName", textBoxLastName.Text);
+                        cmd.Parameters.AddWithValue("@FirstName", textBoxFirstName.Text);
+                        cmd.Parameters.AddWithValue("@Age", int.Parse(textBoxAge.Text));
+                        cmd.Parameters.AddWithValue("@Address", textBoxAddress.Text);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            ClearInputs();
+                            LoadPersonsData();
+                            MessageBox.Show("Person added successfully!");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error adding person: {ex.Message}");
+                }
+            }
+
+        }
+
+        private bool ValidateInputs()
+        {
+            if (string.IsNullOrWhiteSpace(textBoxLastName.Text))
+            {
+                MessageBox.Show("Please enter a last name.");
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(textBoxFirstName.Text))
+            {
+                MessageBox.Show("Please enter a first name.");
+                return false;
+            }
+
+            if (!int.TryParse(textBoxAge.Text, out int age) || age <= 0)
+            {
+                MessageBox.Show("Please enter a valid age.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void ClearInputs()
+        {
+            textBoxId.Clear();
+            textBoxLastName.Clear();
+            textBoxFirstName.Clear();
+            textBoxAge.Clear();
+            textBoxAddress.Clear();
+        }
+
+        private void buttonModify_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxId.Text))
+            {
+                MessageBox.Show("Please select a person to modify.");
+                return;
+            }
+
+            if (ValidateInputs())
+            {
+                try
+                {
+                    using (SqlConnection cmdConnection = new SqlConnection(connectionString))
+                    {
+                        cmdConnection.Open();
+                        SqlCommand cmd = new SqlCommand(
+                            "UPDATE Persons SET LastName = @LastName, FirstName = @FirstName, " +
+                            "Age = @Age, Address = @Address WHERE Id = @Id", cmdConnection);
+
+                        cmd.Parameters.AddWithValue("@LastName", textBoxLastName.Text);
+                        cmd.Parameters.AddWithValue("@FirstName", textBoxFirstName.Text);
+                        cmd.Parameters.AddWithValue("@Age", int.Parse(textBoxAge.Text));
+                        cmd.Parameters.AddWithValue("@Address", textBoxAddress.Text);
+                        cmd.Parameters.AddWithValue("@Id", int.Parse(textBoxId.Text));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            ClearInputs();
+                            LoadPersonsData();
+                            MessageBox.Show("Person updated successfully!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("No rows were updated.");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error updating person: {ex.Message}");
+                }
+            }
+        }
+
+        private void buttonDelete_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxId.Text))
+            {
+                MessageBox.Show("Please select a person to delete.");
+                return;
+            }
+
+            if (MessageBox.Show("Are you sure you want to delete this person?", "Confirm Delete",
+                MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                try
+                {
+                    using (SqlConnection cmdConnection = new SqlConnection(connectionString))
+                    {
+                        cmdConnection.Open();
+                        SqlCommand cmd = new SqlCommand(
+                            "DELETE FROM Persons WHERE Id = @Id", cmdConnection);
+
+                        cmd.Parameters.AddWithValue("@Id", int.Parse(textBoxId.Text));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            ClearInputs();
+                            LoadPersonsData();
+                            MessageBox.Show("Person deleted successfully!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("No rows were deleted.");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error deleting person: {ex.Message}");
+                }
+            }
+        }
+
+        private void buttonCancel_Click_1(object sender, EventArgs e)
+        {
+            ClearInputs();
+            dataGridViewPersons.ClearSelection();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
